cf5eb12b13faf042a4d77a195ee6b925477795ce
Tatu Saloranta <tsaloranta@gmail.com>
2012-02-19 13:52:58 -0800

    refactoring

--- src/main/java/com/fasterxml/jackson/datatype/hppc/HppcModule.java
+++ src/main/java/com/fasterxml/jackson/datatype/hppc/HppcModule.java
@@ -3,15 +3,11 @@ package com.fasterxml.jackson.datatype.hppc;
 import java.lang.reflect.Type;
 
 import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.deser.Deserializers;
-import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.Serializers;
-import com.fasterxml.jackson.databind.ser.std.ObjectArraySerializer;
 import com.fasterxml.jackson.databind.type.*;
 
 import com.carrotsearch.hppc.ObjectContainer;
-import com.fasterxml.jackson.datatype.hppc.deser.HppcContainerDeserializers;
+import com.fasterxml.jackson.datatype.hppc.deser.HppcDeserializers;
 import com.fasterxml.jackson.datatype.hppc.ser.*;
 
 public class HppcModule extends SimpleModule
@@ -55,82 +51,6 @@ public class HppcModule extends SimpleModule
             }
             return type;
         }
-    }
-    
-    static class HppcSerializers extends Serializers.Base
-    {
-        public HppcSerializers() { }
-        
-        @Override
-        public JsonSerializer<?> findCollectionLikeSerializer(SerializationConfig config,
-                CollectionLikeType containerType,
-                BeanDescription beanDesc, TypeSerializer elementTypeSerializer,
-                JsonSerializer<Object> elementValueSerializer)
-        {
-            if (ObjectContainer.class.isAssignableFrom(containerType.getRawClass())) {
-                // hmmh. not sure if we can find 'forceStaticTyping' anywhere...
-                boolean staticTyping = config.isEnabled(MapperFeature.USE_STATIC_TYPING);
-                ObjectArraySerializer ser = new ObjectArraySerializer(containerType.getContentType(),
-                        staticTyping, elementTypeSerializer, elementValueSerializer);
-                return new ObjectContainerSerializer(containerType, ser);
-            }
-            return null;
-        }
-
-        @Override
-        public JsonSerializer<?> findMapLikeSerializer(SerializationConfig config,
-                MapLikeType arg1, BeanDescription arg2,
-                JsonSerializer<Object> arg4, TypeSerializer arg5,
-                JsonSerializer<Object> arg6) {
-            // TODO: handle XxxMap with Object keys and/or values
-            return null;
-        }
-
-        /**
-         * Anything that we don't explicitly mark as Map- or Collection-like
-         * will end up here...
-         */
-        @Override
-        public JsonSerializer<?> findSerializer(SerializationConfig config,
-                JavaType type, BeanDescription beanDesc)
-        {
-            return HppcContainerSerializers.getMatchingSerializer(type);
-        }
-        
-    }
-
-    static class HppcDeserializers extends Deserializers.Base
-    {
-        @Override
-        public JsonDeserializer<?> findBeanDeserializer(JavaType type,
-                DeserializationConfig config, BeanDescription beanDesc)
-            throws JsonMappingException
-        {
-            return HppcContainerDeserializers.findDeserializer(config, type);
-        }
-
-        /*
-        @Override
-        public JsonDeserializer<?> findCollectionLikeDeserializer(
-                CollectionLikeType type, DeserializationConfig config,
-                 BeanDescription arg3,
-                BeanProperty arg4, TypeDeserializer arg5,
-                JsonDeserializer<?> arg6) throws JsonMappingException {
-            // TODO Auto-generated method stub
-            return null;
-        }
-
-        @Override
-        public JsonDeserializer<?> findMapLikeDeserializer(MapLikeType type,
-                DeserializationConfig config,
-                BeanDescription arg3, BeanProperty arg4, KeyDeserializer arg5,
-                TypeDeserializer arg6, JsonDeserializer<?> arg7)
-                throws JsonMappingException {
-            // TODO Auto-generated method stub
-            return null;
-        }
-*/        
-        
     }
     
 }
